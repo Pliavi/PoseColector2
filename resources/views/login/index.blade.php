001fd8159bfc51e2b52ea051c4e5e5d02dd054b8
Vitor "Pliavi" Silvério de Souza <Pliavi.rpgm@gmail.com>
2017-05-06 20:08:48 -0300

    Update index.blade.php

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -3,10 +3,10 @@
 @section('content')
   <form method="POST" action="{{ route('login') }}" class="siimple-h6" style="text-align: center">
     {{ csrf_field() }}
-    Olá, eu me chamo <input type="text" class="siimple-input" placeholder="Fulano da Silva" name="volunteer" autofocus required>!
+    Olá, eu me chamo <input type="text" class="siimple-input" placeholder="Fulano da Silva" name="volunteer" value="{{ old('volunteer') }}" autofocus required>!
     <br>
     Informe o número fornecido: 
-    <input type="number" class="siimple-input" name="folder" required>
+    <input type="number" class="siimple-input" name="folder" required value="{{ old('folder') }}">
     <br>
     <div style="text-align: center">
       <button class="siimple-btn siimple-btn--green go-button" id="done" type="submit">Começar!</button>
